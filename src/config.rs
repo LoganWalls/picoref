bb1db1fd1b866655eed95400eb47febc9719f946
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2023-07-30 19:02:01 -0400

    add config file

---
+++ src/config.rs
@@ -0,0 +1,33 @@
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct Config {
+    /// Path to library
+    pub root: Option<PathBuf>,
+}
+
+pub fn load(path: Option<PathBuf>) -> Result<Config> {
+    let fallback_path = || {
+        let mut fb_path = std::env::var("XDG_CONFIG_HOME")
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| {
+                [
+                    std::env::var("HOME").expect("No config paths found"),
+                    ".config".to_string(),
+                ]
+                .iter()
+                .collect()
+            });
+        fb_path.push("picoref");
+        fb_path.push("config.toml");
+        fb_path
+    };
+
+    Ok(toml::from_str(&read_to_string(
+        path.unwrap_or_else(fallback_path),
+    )?)?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 mod citekey;
+mod config;
 mod fetch;
 mod regex;
+use std::path::PathBuf;
 use anyhow::Result;
-use clap::{command, Parser};
+use clap::{command, Parser, ValueHint};
 
 #[derive(Parser, Clone, Default, Debug)]
 #[command(author, version, about, long_about = None)]
 struct CliArgs {
     /// The DOI of the reference to fetch
     #[arg(short, long)]
-    pub doi: String,
+    pub doi: Option<String>,
+
+    /// Path to the configuration file that should be used
+    #[arg(short, long, value_name = "FILE", value_hint = ValueHint::FilePath)]
+    pub config: Option<PathBuf>,
+
+    /// Path to the root of the library to use (defaults to path specified in config)
+    #[arg(short, long, value_name = "FILE", value_hint = ValueHint::FilePath)]
+    pub root: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
